ee11064ddd0af9c74f48ff2448d90152157ef401
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-05-17 22:57:18 +0530

    code modification

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n'); }).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
+var http      = require('http');
+
+var express   = require('express');
+
+var app    = express();
+
+
+app.get('/', function(req, res){
+  
+    res.send("Hello World!");
+  
+});
+
+console.log('starting the Express (NodeJS) Web server');
+app.listen(3000);
+console.log('Webserver is listening on port 3000');
