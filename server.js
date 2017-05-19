c674273cfd16ceaddb16996750bc90f6e9770c40
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-05-19 21:43:29 +0530

    make dynamic addition of users in user list

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http      = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var fs = require("fs");
 
@@ -11,6 +12,9 @@ var user = {
       "id": 4
    }
 }
+
+// parse application/json
+    app.use(bodyParser.json()); 
 
 app.get('/listUsers', function (req, res) {
 
@@ -33,11 +37,24 @@ app.get('/:id', function (req, res) {
 
 
 app.post('/addUser', function (req, res) {
+
+   reqJson =  req.body;
+   
    // First read existing users.
    fs.readFile("./" + "users.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
-       data["user4"] = user["user4"];
+    
+       var keys = Object.keys(reqJson);
+       for (var i = 0; i < keys.length; i++) {
+            data[keys[i]] = reqJson[keys[i]]
+       }
+       
        console.log( data );
+       json = JSON.stringify(data);
+       fs.writeFile("./" + "users.json", json, 'utf8',function(err){
+          if(err) throw err;
+       });
+       
        res.end( JSON.stringify(data));
    });
 })
