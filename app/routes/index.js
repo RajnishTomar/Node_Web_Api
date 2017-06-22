9e8eb53694d3758e4f8d24b3fa225f2b2c485cdc
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-06-23 01:15:35 +0530

    creted new variable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 const merchantRoute = require('./merchant');
-const utilityRoute = require('./utility');
+//const utilityRoute = require('./utility');
 
 module.exports = function(app, db) {
   merchantRoute(app, db);
-  utilityRoute(app, db);
+  //utilityRoute(app, db);
   // Other route groups could go here, in the future
 };
