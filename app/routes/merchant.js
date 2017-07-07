ece34b71cb63ed60b3fc4231a484f2487329dda2
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-07-07 18:42:17 +0530

    ne mod

--- app/routes/merchant.js
+++ app/routes/merchant.js
@@ -359,16 +359,7 @@ app.post('/postImage', function (req, res) {
     req.on('end', function (){
         wstream.end();
         console.log('File saved.')
-        
-             
-             // fs.unlink(sourceUrl, function(status){
-//              });
-             var result = [];
-             var dict = {"status": "true", "message":"Added successfully", "image_url":filePath};
-             result.push(dict)
-             res.end( JSON.stringify(result));
-          
-             //uploadToS3(nm,filePath, res);  
+        uploadToS3(nm,filePath, res);  
     });
 
 })
