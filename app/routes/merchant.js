821fa2a01ebae313e497a6b16be7451ab0de4d49
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-07-07 17:48:34 +0530

    new keys

--- app/routes/merchant.js
+++ app/routes/merchant.js
@@ -342,8 +342,8 @@ app.post('/editMerchantProductCategoryItems', function (req, res) { //to add mor
 app.post('/postImage', function (req, res) {
 
      AWS.config.update({
-      accessKeyId: "AKIAJKV3HUTTGQW7DLHA",
-      secretAccessKey: "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",
+      accessKeyId: "AKIAJKV3HUTTGQW7DLHA",  // old access key : AKIAJKV3HUTTGQW7DLHA
+      secretAccessKey: "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",  //od secret key : iY2S12wVq753KT0xYNJstQlPE49ACQWNWjwo9oUU
       "region": "ap-south-1" 
     });
     
