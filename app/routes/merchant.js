fdf281cf34b3732eb400ccae9b9d5a6086052f9a
Rajnish Tomar <tomar.rajnish@yahoo.com>
2017-07-07 19:07:03 +0530

    dynamic keys

--- app/routes/merchant.js
+++ app/routes/merchant.js
@@ -341,9 +341,13 @@ app.post('/editMerchantProductCategoryItems', function (req, res) { //to add mor
 
 app.post('/postImage', function (req, res) {
 
+     var requestHeaders =  req.headers;
+     const accessKey = requestHeaders['access-key'];
+     const secretKey = requestHeaders['secret-key'];
+     console.log();
      AWS.config.update({
-      accessKeyId: "AKIAJTNSHGI3SNSL3P2Q",  // old access key : AKIAJKV3HUTTGQW7DLHA
-      secretAccessKey: "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",  //od secret key : iY2S12wVq753KT0xYNJstQlPE49ACQWNWjwo9oUU
+      accessKeyId: accessKey,  // old access key : AKIAJKV3HUTTGQW7DLHA
+      secretAccessKey: secretKey,  //od secret key : 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
       "region": "ap-south-1" 
     });
     
